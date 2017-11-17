2ef33708552118e412b1b3603a63705b9c8635b2
Matthew Dworkin <mdworkin21@gmail.com>
2017-11-17 21:49:48 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -5,7 +5,7 @@ const db = require("../config/db")
 class Question{
 
   static CreateTable(){
-    lreturn new Promise(function(resolve){
+    return new Promise(function(resolve){
       const sql = `SELECT * FROM sqlite_master WHERE type = 'table' AND name = ?`
       db.get(sql, [ tableName ], function(err, row){
         resolve(row);
