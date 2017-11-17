d087b908ee062cf04a5a439ab1933582dc79510f
Matthew Dworkin <mdworkin21@gmail.com>
2017-11-17 21:49:32 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -5,16 +5,13 @@ const db = require("../config/db")
 class Question{
 
   static CreateTable(){
-    let getTableInfo = new Promise(function (resolve){
+    lreturn new Promise(function(resolve){
       const sql = `SELECT * FROM sqlite_master WHERE type = 'table' AND name = ?`
       db.get(sql, [ tableName ], function(err, row){
-          resolve(row)})
-
-          resolve(getTableInfo)
-
-  })
-
-}}
+        resolve(row);
+      });
+    })
+  }}
 
 
 module.exports = Question;
